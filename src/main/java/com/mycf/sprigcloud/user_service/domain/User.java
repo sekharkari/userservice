6724779af80752f2a04272da9b384812f47a6527
Sekhar Kari <sekhar.kari@gmail.com>
2017-02-08 09:30:32 +0530

    adding domain class user

--- src/main/java/com/mycf/sprigcloud/user_service/domain/User.java
+++ src/main/java/com/mycf/sprigcloud/user_service/domain/User.java
@@ -3,11 +3,21 @@ package com.mycf.sprigcloud.user_service.domain;
 public class User {
 	private String name;
 	private int userId;
+	private String emailAddress;
 
 	public User(String name, int userId) {
 		super();
 		this.name = name;
 		this.userId = userId;
+		
+	}
+	
+	public User(String name, int userId,String emailAddress) {
+		super();
+		this.name = name;
+		this.userId = userId;
+		this.emailAddress=emailAddress;
+		
 	}
 
 	public String getName() {
@@ -24,6 +34,14 @@ public class User {
 
 	public void setUserId(int userId) {
 		this.userId = userId;
+	}
+
+	public String getEmailAddress() {
+		return emailAddress;
+	}
+
+	public void setEmailAddress(String emailAddress) {
+		this.emailAddress = emailAddress;
 	}
 
 }
