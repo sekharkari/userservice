79950675ff475390a1cffb047fd29dad794a8811
Sekhar Kari <sekhar.kari@gmail.com>
2017-02-08 09:48:19 +0530

    added new test

---
+++ src/test/java/com/mycf/sprigcloud/user_service/domain/UserTest.java
@@ -0,0 +1,31 @@
+package com.mycf.sprigcloud.user_service.domain;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class UserTest {
+
+	@Test
+	public void testConstructor() {
+		User u=new User("name", 1,"name@gmail.com");
+		assertEquals("name",u.getName());
+		assertEquals(1,u.getUserId());
+		assertEquals("name@gmail.com",u.getEmailAddress());
+	}
+	
+	@Test
+	public void testEmptyConstructor() {
+		User u=new User();
+		String name="name";
+		int userId=1;
+		String emailAddress="email";
+		u.setName(name);
+		u.setUserId(userId);
+		u.setEmailAddress(emailAddress);
+		assertEquals("name",u.getName());
+		assertEquals(1,u.getUserId());
+		assertEquals("name@gmail.com",u.getEmailAddress());
+	}
+
+}
