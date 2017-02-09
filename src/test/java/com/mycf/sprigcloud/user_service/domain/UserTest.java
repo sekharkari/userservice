0d6ec2a163952a9aa8cd646c5c52b34fa19ef0f0
Sekhar Kari <sekhar.kari@gmail.com>
2017-02-09 15:11:25 +0530

    added more testcases

--- src/test/java/com/mycf/sprigcloud/user_service/domain/UserTest.java
+++ src/test/java/com/mycf/sprigcloud/user_service/domain/UserTest.java
@@ -1,41 +1,57 @@
 package com.mycf.sprigcloud.user_service.domain;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
 
 import org.junit.Test;
 
 public class UserTest {
 
 	@Test
-	public void testConstructor() {
-		User u=new User("name", 1,"name@gmail.com");
-		assertEquals("name",u.getName());
-		assertEquals(1,u.getUserId());
-		assertEquals("name@gmail.com",u.getEmailAddress());
+	public void testDefaultConstructor() {
+		User u = new User();
+		assertNull(u.getName());
+		assertEquals(0, u.getUserId());
+		assertNull(u.getEmailAddress());
+		assertNull(u.getPhoneNumber());
 	}
-	
+
+	@Test
+	public void testConstructor() {
+		User u = new User("name", 1, "name@gmail.com");
+		assertEquals("name", u.getName());
+		assertEquals(1, u.getUserId());
+		assertEquals("name@gmail.com", u.getEmailAddress());
+	}
+
 	@Test
 	public void testConstructor2() {
-		User u=new User("name2", 2,"name2@gmail.com");
-		assertEquals("name2",u.getName());
-		assertEquals(2,u.getUserId());
-		assertEquals("name2@gmail.com",u.getEmailAddress());
+		String name = "name";
+		int userId = 1;
+		String emailAddress = "email";
+		String phoneNumber = "111-222-3333";
+		User u = new User(name, userId, emailAddress, phoneNumber);
+		assertEquals(name, u.getName());
+		assertEquals(userId, u.getUserId());
+		assertEquals(emailAddress, u.getEmailAddress());
+		assertEquals(phoneNumber, u.getPhoneNumber());
 	}
-	
+
 	@Test
 	public void testEmptyConstructor() {
-		User u=new User();
-		String name="name";
-		int userId=1;
-		String emailAddress="email";
-		String phoneNumber="111-222-3333";
+		User u = new User();
+		String name = "name";
+		int userId = 1;
+		String emailAddress = "email";
+		String phoneNumber = "111-222-3333";
 		u.setName(name);
 		u.setUserId(userId);
 		u.setEmailAddress(emailAddress);
-		assertEquals(name,u.getName());
-		assertEquals(userId,u.getUserId());
-		assertEquals(emailAddress,u.getEmailAddress());
-		assertEquals(phoneNumber,u.getPhoneNumber());
+		u.setPhoneNumber(phoneNumber);
+		assertEquals(name, u.getName());
+		assertEquals(userId, u.getUserId());
+		assertEquals(emailAddress, u.getEmailAddress());
+		assertEquals(phoneNumber, u.getPhoneNumber());
 	}
 
 }
